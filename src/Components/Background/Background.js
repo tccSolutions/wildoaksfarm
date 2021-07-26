eb11e0656490abe8ba8a7406866b0fa5d13f901d
tccSolutions <tim@tccs.tech>
2021-07-26 17:47:47 -0400

    update to background

--- src/Components/Background/Background.js
+++ src/Components/Background/Background.js
@@ -3,7 +3,15 @@ import React from 'react'
 class Background extends React.Component{
     render(){
         return(
-            <div className='background'>
+            <div className='background carousel slide carousel-fade' data-bs-ride="carousel">
+                <div className = 'carousel-inner'>
+                    <div className='carousel-item active'>
+                        <img src='https://img.hobbyfarms.com/breeds_aberdeen.jpg' alt='' className='d-block w-100 photo' />
+                    </div>
+                    <div className='carousel-item'>
+                        <img src="https://img1.wsimg.com/isteam/ip/97287d06-a43b-42e7-910d-8d2b509730ad/DSC_8307.JPG/:/rs=w:1280" alt='' className='d-block w-100 photo'/>
+                    </div>                  
+                </div>
 
             </div>
         );
